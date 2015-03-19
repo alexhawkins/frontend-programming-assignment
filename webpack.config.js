3a9addf8bb9a435fad97d9711e771fcd7dc58abd
Alex Hawkins <alexhawkins.me@gmail.com>
2015-03-18 19:13:15 -0700

    add bootstrap config to webpack

--- webpack.config.js
+++ webpack.config.js
@@ -5,7 +5,11 @@ module.exports = {
   },
   module: {
     loaders: [
-      {test: /\.js$/, loader: 'jsx-loader'}
+      {test: /\.js$/, loader: 'jsx-loader'},
+      { test: /\.woff$/,   loader: "url-loader?limit=10000&minetype=application/font-woff" },
+      { test: /\.ttf$/,    loader: "file-loader" },
+      { test: /\.eot$/,    loader: "file-loader" },
+      { test: /\.svg$/,    loader: "file-loader" }
     ]
   }
 };
