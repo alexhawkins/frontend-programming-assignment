5fa19b5adc763b59a578963a4da1ef38b3d5c92e
Alex Hawkins <alexhawkins.me@gmail.com>
2015-03-18 18:39:47 -0700

    setup actions for dispatcher when making new search requests

---
+++ app/actions/SearchActions.js
@@ -0,0 +1,18 @@
+var AppDispatcher = require("../dispatcher/AppDispatcher");
+var AppConstants = require("../constants/AppConstants");
+var searchYouTubeUtils = require("../utils/SearchYouTubeUtils");
+
+var callback = function(response) {
+    AppDispatcher.handleAction({
+        actionType: AppConstants.GET_VIDEOS,
+        data: response
+    });
+};
+
+var searchActions = {
+    getVideos: function(query) {
+        searchYouTubeUtils.getVideos(query, callback);
+    }
+};
+
+module.exports = searchActions;
