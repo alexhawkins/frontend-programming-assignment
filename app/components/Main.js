896b14f92a2cddbc7547c8b0e76784619f72f39c
Alex Hawkins <alexhawkins.me@gmail.com>
2015-03-18 18:41:15 -0700

    create Main component

---
+++ app/components/Main.js
@@ -0,0 +1,30 @@
+var React = require("react");
+var Router = require("react-router");
+var RouteHandler = Router.RouteHandler;
+var SearchBar = require("./SearchBar/SearchBar");
+var TableContainer = require("./Table/TableContainer");
+
+var Main = React.createClass({
+
+    render: function(){
+        return (
+            <div className="main-container">
+                <nav className="navbar navbar-default" role="navigation">
+                    <div className="col-sm-7 col-sm-offset-2 search-bar-padding">
+                        <SearchBar />
+                    </div>
+                </nav>
+                <div className="container">
+                    <div className="intro-title">Can't find a video to watch?
+                        <span className="main-title">
+                            &nbsp;Sort through your favorites below!
+                        </span>
+                    </div>
+                    <RouteHandler/>
+                </div>
+            </div>
+        );
+    }
+});
+
+module.exports = Main;
